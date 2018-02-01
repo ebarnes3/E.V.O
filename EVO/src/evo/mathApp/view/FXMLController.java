a02d357e342870438b5b46af234724c227c64cfd
Kevin Haro <kharo1@student.gsu.edu>
2018-02-01 18:55:28 -0500

    FXMLController added

--- EVO/src/evo/mathApp/view/FXMLController.java
+++ EVO/src/evo/mathApp/view/FXMLController.java
@@ -4,11 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class FXMLController {
+    private boolean displayKeyboard = false;
+
     public void initialize(){
 
     }
     @FXML
     protected void keyboardClicked(ActionEvent event) {
-        
+        if(displayKeyboard == false){
+            displayKeyboard = true;
+            
+        }
+        else if(displayKeyboard == true){
+            displayKeyboard = false;
+
+        }else{
+            System.out.println("Boolean error with displayKeyboard");
+        }
     }
 }
